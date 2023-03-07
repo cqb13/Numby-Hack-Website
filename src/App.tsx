c41cb667127a39dbec0271aef38e9f49fb62e345
cqb13 <cqb13.main@gmail.com>
2023-03-06 22:27:18 -0500

    📄 Space between sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import Features from './components/features/features'
-import Hero from './components/hero/hero'
-import Nav from './components/nav/nav'
-import FAQ from './components/faq/faq'
+import Features from "./components/features/features";
+import Hero from "./components/hero/hero";
+import Nav from "./components/nav/nav";
+import FAQ from "./components/faq/faq";
 
 function App() {
   return (
     <main>
       <Nav />
       <Hero />
+      <section className="empty-space" />
       <Features />
+      <section className="empty-space" />
       <FAQ />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
