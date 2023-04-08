dfaed9a9141d83780983830b0a971d85f1758e76
cqb13 <cqb13.main@gmail.com>
2023-04-08 13:51:35 -0400

    🔧 Fixed Icon

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -4,7 +4,7 @@ import "./nav.css";
 const Nav = () => {
   return (
     <nav className={useScroll("scrollOffSet", 10) ? "scrolled" : ""}>
-      <img src="../../../public/icon.png" alt="icon" className="icon" />
+      <img src="https://numbyhack.vercel.app/icon.png" alt="icon" className="icon" />
       <a href="#home">Home</a>
       <a href="#features">Features</a>
       <a href="#faq">FAQ</a>
